43d11da95e751a5ba5f7ef98170077065c5119f6
tlivengood <tracy.livengood@gmail.com>
2014-03-12 22:33:57 -0600

    Get started with d3

--- public/js/chart.js
+++ public/js/chart.js
@@ -10,6 +10,30 @@ define(["backbone", "jquery", "underscore", "d3"], function(Backbone, $, _, d3) 
         updateChart: function() {
             var data = this.loans.getChartData();
 
+            var totalWidth = this.$el.width(),
+                totalHeight = this.$el.height();
+
+            //Need actual coordinates to calculate this data. Use underscore for transformations.
+            var years = _.keys(data),
+                yearWidth = totalWidth / years.length;
+
+            //Draw the axes (need to rewrite to use d3 new axis feature)
+            d3.select(this.el)
+                .append('line')
+                .attr('x1', 0)
+                .attr('y1', totalHeight)
+                .attr('x2', totalWidth)
+                .attr('y2', totalHeight)
+                .attr('class', 'axis');
+
+            d3.select(this.el)
+                .append('line')
+                .attr('x1', 0)
+                .attr('y1', totalHeight)
+                .attr('x2', 0)
+                .attr('y2', 0)
+                .attr('class', 'axis');
+
         }
     });
 });
